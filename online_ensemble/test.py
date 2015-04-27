a69723a47dc20fd0fa2d55ebc16d46fe20e5e85e
dejiyu <dejiyu@gmail.com>
2015-04-26 22:42:55 -0400

    test parallelization

--- online_ensemble/test.py
+++ online_ensemble/test.py
@@ -1,61 +1,41 @@
 import matplotlib
 matplotlib.use('Agg')
-import master_classification as my
+import anil_master_fast_convergence as my
 import wang_algo as wang
 import single as single
 import matplotlib.pyplot as plt
 import time
 import datetime
-d = 10  # the number of dimensions
-kk = 0.2  # the number of changing dimensions
-t = 0.1  # the magnitude of the change
-for kk in [0.2, 0.4, 0.6]:
-    chunk_size = 1000   # the size of chunk
-    n_classifiers = 8   # the number of classifiers
-    n_test_chunks = 900
-    name_prefix = 'd' + str(d) + '_k' + str(kk) + '_t' + str(t)
-    data_file_name = name_prefix + '.in'
-    target_file_name = name_prefix + '.out'
-    outFile = open('results/' + name_prefix + '.txt', 'w')
 
-    for chunk_size in [1000]:
-        for n_classifiers in [2, 8, 16]:
-            n_test_chunks = 900 * 1000 / chunk_size
-            print '------------------', chunk_size, n_classifiers
+outFile = open('result/result.txt', 'w')
+for n_jobs in [1]:
+    for chunk_size in [200]:
+        for n_trees in [200]:
+            name_prefix = str(n_jobs) + '_' + str(chunk_size) + '_' + str(n_trees)
+            outFile2 = open('result/' + name_prefix + '.txt', 'w')
+            print 'n_jobs=', n_jobs, 'chunk_size=', chunk_size, 'n_trees=', n_trees
 
             plt.clf()
 
-            start_time = time.time()
-            error_rate, error_rate_vec = my.run_classification(data_file_name, target_file_name, n_test_chunks, chunk_size, n_classifiers, 'random_forest_regressor')
-            end_time = time.time()
-            delta = str(int(end_time - start_time))
-            plt.plot(error_rate_vec)
-            outFile.write(str(chunk_size) + ' ' + str(n_classifiers) + ' ' + str(error_rate) + ' O ' + delta + ' \n')
+            mse_vec, rep_vec, mse, exe_time = my.test(n_jobs=n_jobs, chunk_size=chunk_size, n_trees=n_trees, category='random_forest_regressor')
+            plt.plot(mse_vec)
+            outFile.write(name_prefix + '_' + 'RF_' + str(exe_time) + ' \n')
+            outFile2.write(name_prefix + '_' + 'RF_' + str(exe_time) + ' \n')
 
-            start_time = time.time()
-            error_rate, error_rate_vec = wang.run_classification(data_file_name, target_file_name, n_test_chunks, chunk_size, n_classifiers)
-            end_time = time.time()
-            delta = str(int(end_time - start_time))
-            plt.plot(error_rate_vec)
-            outFile.write(str(chunk_size) + ' ' + str(n_classifiers) + ' ' + str(error_rate) + ' W ' + delta + ' \n')
-            """
-            start_time = time.time()
-            error_rate, error_rate_vec = single.run_classification(data_file_name, target_file_name, n_test_chunks, chunk_size, n_classifiers)
-            end_time = time.time()
-            delta = str(int(end_time - start_time))
-            plt.plot(error_rate_vec)
-            outFile.write(str(chunk_size) + ' ' + str(n_classifiers) + ' ' + str(error_rate) + ' S ' + delta + ' \n')
-            """
-            start_time = time.time()
-            error_rate, error_rate_vec = my.run_classification(data_file_name, target_file_name, n_test_chunks, chunk_size, n_classifiers, 'combination')
-            end_time = time.time()
-            delta = str(int(end_time - start_time))
-            plt.plot(error_rate_vec)
-            outFile.write(str(chunk_size) + ' ' + str(n_classifiers) + ' ' + str(error_rate) + ' C ' + delta + ' \n')
+            mse_vec, rep_vec, mse, exe_time = my.test(n_jobs=n_jobs, chunk_size=chunk_size, n_trees=n_trees, category='svm_regressor')
+            plt.plot(mse_vec)
+            outFile.write(name_prefix + '_' + 'SVM_' + str(exe_time) + ' \n')
+            outFile2.write(name_prefix + '_' + 'SVM_' + str(exe_time) + ' \n')
 
-            plt.legend(['svm', 'wang', 'combination'], loc='upper right')
+            mse_vec, rep_vec, mse, exe_time = my.test(n_jobs=n_jobs, chunk_size=chunk_size, n_trees=n_trees, category='combination')
+            plt.plot(mse_vec)
+            outFile.write(name_prefix + '_' + 'COM_' + str(exe_time) + ' \n')
+            outFile2.write(name_prefix + '_' + 'COM_' + str(exe_time) + ' \n')
+
+            outFile2.close()
+
+            plt.legend(['RF', 'SVM', 'COMB'], loc='upper right')
             plt.title(name_prefix)
             plt.show()
-            plt.savefig('results/' + name_prefix + '/ch' + str(chunk_size) + '_n' + str(n_classifiers) + '.png')
-
-    outFile.close()
+            plt.savefig('result/' + name_prefix + '.png')
+outFile.close()
